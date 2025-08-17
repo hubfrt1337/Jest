df570364ff349adcdbb6945061cd81e4a00d8306
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-08-17 15:06:42 +0200

    test: Add jest test for calculator methods and invalid inputs (TDD)

---
+++ calculator.test.js
@@ -0,0 +1,13 @@
+import {calculator} from "./calculator"
+test("Add 1.2 + 2 is equal to 3", () => {
+    expect(calculator.add(1.2,2.4)).toBeCloseTo(3.6,5);
+})
+test("multiply 4 x 0.5 is equal to 2", () => {
+    expect(calculator.multiply(4, 0.5)).toBeCloseTo(2)
+})
+test("return falsy if value is undefined", () => {
+    expect(calculator.subtract(undefinec, 2)).toBeFalsy()
+})
+test("Return falsy if input is not a number", () => {
+    expect(calculator.add()).toBeFalsy();
+})
