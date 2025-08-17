7ff96a8d158e9b5b1248067c5635d1b622678157
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-08-17 10:27:45 +0200

    test: Add tests to reverseString function. (TDD)

---
+++ reverseString.test.js
@@ -0,0 +1,15 @@
+import {reverseString} from ("./reverseString");
+
+test("It reverses a string", () => {
+    expect(reverseString("pizza")).toBe("azzip");
+})
+
+test("Return undefined when input is undefined", () => {
+    expect(reverseString(b)).toBeUndefined();
+})
+test("return false when input is number", () => {
+    expect(reverseString(123)).toBeFalsy();
+})
+test("works with empty string", () => {
+    expect(reverseString("")).toBe("");
+})
